bd1f1cdee5ca44be435885d9b5874d1988bb25ea
Sherin <62134039+sherinakbar@users.noreply.github.com>
2020-03-13 19:45:27 +0530

    Create simple interest.c

---
+++ simple interest.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+  {
+     float principle,time,rate,SI;
+     printf("enter principle(amount):");
+     scanf("%f",&principle);
+     printf("enter time:");
+     scanf("%f",&time):
+     printf("enter rate:);
+     scanf("%f",&rate);
+     SI=(rinciple*time*rate)/100;
+     printf("simple interest=%f",SI);
+return 0;
+   }
